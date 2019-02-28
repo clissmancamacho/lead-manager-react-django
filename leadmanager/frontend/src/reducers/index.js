d6fa2b00d11bd2e1766ce2bf16bed8cba17eb882
Clissman J. Camacho <clissmancamacho@gmail.com>
2019-02-28 10:52:44 -0400

    first commit

---
+++ leadmanager/frontend/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import leadsReducer from "./leadsReducer";
+import errorsReducer from "./errorsReducer";
+import messagesReducer from "./messagesReducer";
+import authReducer from "./authReducer";
+
+export default combineReducers({
+  leadsReducer,
+  errorsReducer,
+  messagesReducer,
+  authReducer
+});
